6f826368d017babd503d9f5f492c451d29098544
Ângela Cavunduri <angelacavundurijulcira@gmail.com>
2025-09-17 16:18:38 +0100

    novo

--- meu-projecto/src/components/pages/NewProject.js
+++ meu-projecto/src/components/pages/NewProject.js
@@ -1,8 +1,16 @@
 import ProjectFrom from '../project/ProjectFrom'
 import styles from './Newproject.module.css'
+import {useHistory} from 'react-router-dom'
 
 
 function NewProject(){
+    const history=useHistory()
+
+    function Createpost(){
+        //inicialização cost and services
+        
+    }
+
     return(
         <div className={styles.newproject_container}>
             <h1>Criar projevtos</h1>
